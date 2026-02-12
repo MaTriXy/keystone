96a6c883a8dacca01f32e7c28c8a423952efec55
Thad Hughes <thad@imbue.com>
2026-02-11 20:24:04 -0400

    Remove modal_registry/conftest.py that shadowed bootstrap_devcontainer conftest
    The modal_registry conftest was causing 'cannot import SAMPLES_DIR from conftest'
    errors. The manual marker is already registered in pytest.ini.

--- modal_registry/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-def pytest_configure(config: pytest.Config) -> None:
-    config.addinivalue_line(
-        "markers",
-        "manual: test requires external services (registry, docker daemon)",
-    )
