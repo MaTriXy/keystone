6ec49d511f1d300814b65f791b8004fcc66bf28f
Thad Hughes <thad@imbue.com>
2026-02-18 10:04:53 -0800

    fixmes

--- keystone/src/keystone/__init__.py
+++ keystone/src/keystone/__init__.py
@@ -1,6 +1,6 @@
 """Bootstrap devcontainer - automatically generates working .devcontainer/ setups."""
 
-# FIXME: Delete this -- these __init__.py files are annoying.
+# FIXME: Delete this -- these __init__.py files are annoying and legacy.
 
 from keystone.keystone_cli import main
 
